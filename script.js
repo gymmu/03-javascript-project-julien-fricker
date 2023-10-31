5335b955f01f39c87d76a32b5512987142d8bace
Julien Fricker <e227505@edu.sbl.ch>
2023-10-31 10:46:28 +0100

    Aufgabe 2 erstellt

--- script.js
+++ script.js
@@ -6,11 +6,25 @@ export function aufgabe01(args) {
     const currentElement = input[i]
   
   if (currentElement === "e") {
-    result.push("")
-  } else {
+    result.push("") }
+    else if (currentElement === "E") {
+      result.push("")
+    }
+
+   else {
     result.push(currentElement)
   }
-  
+ 
+ } 
+  return result.join("")
+} 
+  export function aufgabe02 (args) {
+  const input = args
+  const result = []
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    
   }
   return result.join("")
 }
+
