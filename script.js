2fc625f30066a53744672c699347adcc8e400597
Julien Fricker <e227505@edu.sbl.ch>
2023-12-14 20:48:54 +0100

    Versuch Aufgabe 4

--- script.js
+++ script.js
@@ -55,9 +55,43 @@ countE++
 export function aufgabe04 (args) {
   const input = args
   const result = []
+
+  const cleanText = []
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
-    
+    const ascii = currentElement.charCodeAt(0)
+  if(32 === ascii) {
+    cleanText.push(currentElement)
+  }else if(65 <= ascii && ascii <= 90) {
+    cleanText.push(currentElement)
+  }else if(97 <= ascii && ascii <= 122) {
+    cleanText.push(currentElement)
   }
-  return result.join("")
+  }
+  
+  const tmp = cleanText.join("")
+  const tmpText = []
+
+  for (let i = 0; i < tmp.length; i++) {
+    const currentElement = tmp[i]
+    const nextElement = tmp [i + 1] 
+  if (currentElement === "" && nextElement === " ") {}
+  //ignoriert mehrere Leerzeichen hintereinander
+
+  else {
+    tmpText.push(currentElement)
+  }
+  }
+
+  const clean = tmpText.join("")
+
+  let words = 0
+  for (let i = 0; i < clean.length; i++) {
+    const currentElement = clean[i]
+    if (currentElement === " ") {
+      words++
+    }
+  }
+  return words + 1
+
 }
